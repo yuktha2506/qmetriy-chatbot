5f119340d487a64644e222a90d7f6d41d3c5761a
Yuktha Shree P <yukthashree456@gmail.com>
2026-06-18 11:07:25 +0530

    .

---
+++ QMetrix-UI-main/postcss.config.js
@@ -0,0 +1,14 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
+  plugins: {
+    tailwindcss: {
+      config: join(__dirname, 'tailwind.config.js'),
+    },
+    autoprefixer: {},
+  },
+};
