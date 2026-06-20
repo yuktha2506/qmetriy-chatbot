950b5a54f0af2359bce6ca12b4c291c43192adfd
Yuktha Shree P <yukthashree456@gmail.com>
2026-06-20 14:06:02 +0530

    .

---
+++ QMetrix-UI-main/src/index.jsx
@@ -0,0 +1,24 @@
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import './index.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import App from './App';
+import { store } from './store/store';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const queryClient = new QueryClient();
+
+root.render(
+  <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </Provider>,
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
