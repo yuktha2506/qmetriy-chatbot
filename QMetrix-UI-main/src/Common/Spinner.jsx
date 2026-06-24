4ed57051f98ddf38329a0ce046c2133a66e3958a
Yuktha Shree P <yukthashree456@gmail.com>
2026-06-24 09:29:58 +0530

    .

---
+++ QMetrix-UI-main/src/Common/Spinner.jsx
@@ -0,0 +1,9 @@
+const Spinner = () => {
+    return (
+      <div className="flex items-center justify-center h-screen z-50">
+        <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500 border-solid z-50"></div>
+      </div>
+    );
+  };
+  
+  export default Spinner;
