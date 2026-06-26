747516c36f1cfcd9d979b88172a77155cc4e61db
Yuktha Shree P <yukthashree456@gmail.com>
2026-06-26 15:53:13 +0530

    .

---
+++ QMetrix-UI-main/src/Jira/IssueType/StatusType.jsx
@@ -0,0 +1,181 @@
+import { useState } from 'react';
+import PieChart from '../../Common/PieChart';
+import { useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
+import DropdownButton from '../../Common/DropDown';
+import BarChart from '../../Common/BarGraph';
+import DoughnutChart from '../../Common/DonutChart';
+import LineChart from '../../Common/LineChart';
+
+const TrafficSourceChart = ({ getTaskCountValue }) => {
+  const [selectedChart, setSelectedChart] = useState('pieChart');
+  const rootStyles = getComputedStyle(document.documentElement);
+  const issesTypes =
+    getTaskCountValue.find((item) => item.statusDistribution)?.statusDistribution?.result || [];
+  const filteredIssesTypes = issesTypes.filter(
+    (item) => item.name.toLowerCase() !== 'done' && item.name.toLowerCase() !== 'closed',
+  );
+  const labels = filteredIssesTypes ? filteredIssesTypes.map((item) => item.name) : [];
+  const series = filteredIssesTypes ? filteredIssesTypes.map((item) => item.count) : [];
+  const total = series.reduce((sum, count) => sum + count, 0);
+  const backgroundColors = [
+    rootStyles.getPropertyValue('--pie-multiColor-primary').trim(),
+    rootStyles.getPropertyValue('--pie-multiColor-secondary').trim(),
+    rootStyles.getPropertyValue('--pie-multiColor-tertiary').trim(),
+    rootStyles.getPropertyValue('--pie-multiColor-quaternary').trim(),
+    rootStyles.getPropertyValue('--pie-multiColor-quinary').trim(),
+  ];
+
+  const theme = useSelector((state) => state.theme.theme);
+  const themes = {
+    light: {
+      backgroundColor: 'white',
+      labelColor: rootStyles.getPropertyValue('--label-color-light').trim(),
+      gridColor: rootStyles.getPropertyValue('--grid-color-light').trim(),
+      borderColor: rootStyles.getPropertyValue('--border-color-light').trim(),
+      datalabelsColor: rootStyles.getPropertyValue('--datalabels-color-light').trim(),
+      legendColor: rootStyles.getPropertyValue('--legend-color-light').trim(),
+    },
+    dark: {
+      backgroundColor: '#2f3349',
+      labelColor: rootStyles.getPropertyValue('--label-color-dark').trim(),
+      gridColor: rootStyles.getPropertyValue('--grid-color-dark').trim(),
+      borderColor: rootStyles.getPropertyValue('--border-color-dark').trim(),
+      datalabelsColor: rootStyles.getPropertyValue('--datalabels-color-dark').trim(),
+      legendColor: rootStyles.getPropertyValue('--legend-color-dark').trim(),
+    },
+  };
+
+  const chartOptions = [
+    { label: 'Bar Chart', value: 'barChart' },
+    { label: 'Doughnut Chart', value: 'doughnutChart' },
+    { label: 'Pie Chart', value: 'pieChart' },
+    { label: 'Line Chart', value: 'lineChart'}
+  ];
+  const handleSelect = (option) => {
+    setSelectedChart(option.value);
+  };
+  const customOptions = {
+    plugins: {
+      legend: {
+        display: true,
+        position: 'right',
+        labels: {
+          color: themes[theme].legendColor,
+        },
+      },
+      tooltip: {
+        callbacks: {
+          label: (context) => {
+            const value = context.raw || 0;
+            return `${value}`;
+          },
+        },
+      },
+      datalabels: {
+        color: themes[theme].datalabelsColor,
+        font: {
+          size: 14,
+        },
+      },
+    },
+  };
+
+  const commonChartOptions = {
+    ...customOptions,
+    responsive: true,
+    plugins: {
+      legend: {
+        display: false,
+        position: 'top',
+        labels: {
+          color: themes[theme].legendColor,
+        },
+      },
+      tooltip: {
+        callbacks: {
+          label: (context) => {
+            const value = context.raw || 0;
+            return `${value}`;
+          },
+        },
+      },
+      datalabels: {
+        color: themes[theme].datalabelsColor,
+      },
+    },
+    borderRadius: {
+      topRight: 10,
+      topLeft: 10,
+    },
+  };  
+
+  return (
+    <div className="rounded-md flex flex-col justify-between py-4 dark:bg-[#182433] bg-[#ffffff] dark:text-container text-black">
+      <div className="flex justify-between">
+        <h2 className="text-base font-semibold mb-4 dark:text-container text-black">
+          Total Issues : {total}
+        </h2>
+        <div className="w-auto">
+          <DropdownButton
+            buttonLabel="Select Chart"
+            options={chartOptions}
+            selectedOption={chartOptions.find((option) => option.value === selectedChart)?.label}
+            onSelect={handleSelect}
+            placeholder="Select Chart"
+          />
+        </div>
+      </div>
+      <div className="flex items-center justify-center">
+        {selectedChart === 'pieChart' && (
+          <PieChart
+            labels={labels}
+            dataPoints={series}
+            backgroundColors={backgroundColors}
+            label="Status Type"
+            height="250px"
+            width="400px"
+            options={customOptions}
+          />
+        )}
+        {selectedChart === 'barChart' && (
+          <div className="w-[80%]">
+            <BarChart
+              labels={labels}
+              datasetData={series}
+              datasetLabel={['Status Type']}
+              options={commonChartOptions}
+              height="250px"
+            />
+          </div>
+        )}
+        {selectedChart === 'doughnutChart' && (
+          <DoughnutChart
+            labels={labels}
+            dataPoints={series}
+            label="Status Type"
+            height="250px"
+            width="400px"
+            options={customOptions}
+          />
+        )}
+        {selectedChart === 'lineChart' && (
+          <div className="w-[80%]">
+            <LineChart
+              labels={labels}
+              dataPoints={series}
+              datasetLabel={['Status Type']}
+              options={commonChartOptions}
+              height="250px"
+              tension={0.3}
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+TrafficSourceChart.propTypes = {
+  getTaskCountValue: PropTypes.array.isRequired,
+};
+export default TrafficSourceChart;
