a6999f92bc1d90c46083a4ac9b0a82a0cbedaa13
Yuktha Shree P <yukthashree456@gmail.com>
2026-07-16 12:26:55 +0530

    .

---
+++ QMetrix-API-main/src/modules/stand-up-management/logic/index.js
@@ -0,0 +1,5 @@
+export { default as jiraTableLogic } from './jiraTableLogic.js';
+export { default as storyChurnLogic } from './storyChurnLogic.js';
+export { default as storyChurnExcludingBugsLogic } from './storyChurnExcludingBugsLogic.js';
+export { default as jiraStatusByDevLogic } from './jiraStatusByDevLogic.js';
+export { default as standupBurndownLogic } from './standupBurndownLogic.js';
