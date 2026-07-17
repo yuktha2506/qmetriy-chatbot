dc88eadfd015e225530000ff18b529dfd8e4015b
Yuktha Shree P <yukthashree456@gmail.com>
2026-07-17 14:19:38 +0530

    .

---
+++ QMetrix-API-main/src/modules/test-management/logic/index.js
@@ -0,0 +1,3 @@
+export { default as qaInsightsBugsLogic } from './qaInsightsBugsLogic.js';
+export { default as qaInsightsTestsLogic } from './qaInsightsTestsLogic.js';
+export { default as qaReferenceLogic } from './qaReferenceLogic.js';
