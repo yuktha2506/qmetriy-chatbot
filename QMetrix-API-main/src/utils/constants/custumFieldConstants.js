49cb8f38fc7d409ff03d1794b79add7983f22434
Yuktha Shree P <yukthashree456@gmail.com>
2026-07-19 12:27:52 +0530

    .

---
+++ QMetrix-API-main/src/utils/constants/custumFieldConstants.js
@@ -0,0 +1,27 @@
+export const jiraIssues = ['Story', 'Task', 'Bug', 'Epic', 'Sub-task'];
+export const storyPoints=['story','story points'];
+export const originalEstimates=['original estimate','aggregate original estimates'];
+export const customField = {
+    severity : 'severity',
+};
+export const criticalSeverityKeys = ['critical', 'blocker', 'major'];
+export const blockerSeverityKeys = ['blocker'];
+export const branch=['main','master'];
+export const displayTitle=['prod','production','deploy','deployment'];
+export const developerFieldNames = [
+    'developer',
+    'developer name',
+    'dev name',
+    'developed by',
+];
+export const SPRINT_TYPE = 'sprint';
+export const sub_task_type = 'Sub-task';
+export const epic_type = 'epic';
+export const story_point_type = 'storyPoints';
+export const release_Type = 'release';
+export const issue_Type_Budget_Percentage = {
+    'story': 0.5,
+    'bug': 5,
+    'task': 87.5,
+    'change request': 7,
+};
