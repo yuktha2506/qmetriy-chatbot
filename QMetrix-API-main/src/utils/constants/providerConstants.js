43855a0522bbe54141711acea1424548f4f76266
Yuktha Shree P <yukthashree456@gmail.com>
2026-07-19 12:28:09 +0530

    .

---
+++ QMetrix-API-main/src/utils/constants/providerConstants.js
@@ -0,0 +1,29 @@
+// Provider name constants - normalized (lowercase, no spaces)
+export const PROVIDER_JIRA = 'jira';
+export const PROVIDER_GITHUB = 'github';
+export const PROVIDER_GITLAB = 'gitlab';
+export const PROVIDER_TRELLO = 'trello';
+export const PROVIDER_TESTRAIL = 'testrail';
+export const PROVIDER_XRAY = 'xray';
+export const PROVIDER_ADO = 'ado';
+export const PROVIDER_AZURE_BOARDS = 'azureboards';
+export const PROVIDER_AZURE_BOARD = 'azureboard';
+export const PROVIDER_BITBUCKET = 'bitbucket';
+export const PROVIDER_GITLAB_ISSUES = 'gitlabissues';
+
+// Provider name constants - display names (as stored in database)
+export const PROVIDER_NAME_JIRA = 'Jira';
+export const PROVIDER_NAME_GITHUB = 'Github';
+export const PROVIDER_NAME_GITLAB = 'GitLab';
+export const PROVIDER_NAME_TRELLO = 'Trello';
+export const PROVIDER_NAME_TESTRAIL = 'TestRail';
+export const PROVIDER_NAME_ADO = 'Azure DevOps';
+export const PROVIDER_NAME_AZURE_BOARDS = 'Azure Boards';
+export const PROVIDER_NAME_AZUREBOARDS = 'AzureBoards';
+export const PROVIDER_NAME_AZURE_BOARD = 'AzureBoard';
+export const PROVIDER_NAME_BITBUCKET = 'Bitbucket';
+export const PROVIDER_NAME_GITLAB_ISSUES = 'GitLab Issues';
+export const PROVIDER_NAME_GITHUB_ISSUES = 'GithubIssues';
+
+// API base URL constants
+export const GITLAB_API_BASE_URL = 'https://gitlab.com/api/v4';
